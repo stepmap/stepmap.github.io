03ed05dde1c561785d806cb18455f2246fde02d9
stepmap <70694856+stepmap@users.noreply.github.com>
2020-09-03 12:52:21 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,16 @@
+console.log("asdasddsa");
+
+
+
+// var layerList = document.getElementById('menu');
+// var inputs = layerList.getElementsByTagName('input');
+ 
+// function switchLayer(layer) {
+// var layerId = layer.target.id;
+// map.setStyle('mapbox://styles/mapbox/' + layerId);
+// }
+ 
+// for (var i = 0; i < inputs.length; i++) {
+// inputs[i].onclick = switchLayer;
+// }
+
